15ad0a8dd705dbaf4f84ce3c622653e6b13298e0
DickinsonMark <monoposon@comcast.net>
2016-10-25 11:08:36 -0600

    moved the index.html file into the root folder to push up to gh-pages

--- src/js/config.js
+++ src/js/config.js
@@ -10,11 +10,11 @@
 
   function appConfig($routeProvider) {
     $routeProvider.when('/', {
-      templateUrl: 'js/components/main/main.view.html',
+      templateUrl: 'src/js/components/main/main.view.html',
       controller: 'mainController',
       controllerAs: 'mainCtrl'
     }).when('/checkout', {
-      templateUrl: 'js/components/checkout/checkout.view.html',
+      templateUrl: 'src/js/components/checkout/checkout.view.html',
       controller: 'checkoutController',
       controllerAs: 'checkCtrl'
     });
